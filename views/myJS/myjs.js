c355361d083bd61dc3cf9d5463bb49a51842b1d6
Mindmie <dreamiing.allday@gmail.com>
2017-03-22 18:19:20 +0700

    js login ลองทำเองดูเฉยๆ
    อิอิ

--- views/myJS/myjs.js
+++ views/myJS/myjs.js
@@ -1,5 +1,26 @@
+// modal
 $(document).ready(function(){
     $('#signin').on('shown.bs.modal', function () {
       $('#myInput').focus()
     })
   });
+// login
+$(document).ready(function(){
+    var email,pass;
+    $("#login").click(function(){
+        email=$("#email").val();
+        pass=$("#password").val();
+        $.post("http://localhost:5555/login",{email:email,pass:pass},function(data){  
+          console.log(data);
+          $("#box").html(data);   
+            if(data==='done')           
+            {
+                window.location.href='hello.html';
+            }
+            else
+            {
+              window.location.href='login.html';
+            }
+        });
+    });
+});
